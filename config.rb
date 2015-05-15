50eaa14edf44937164dfd354246a9c7702a41315
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-05-15 21:46:35 +0100

    Separate gem specification

---
+++ config.rb
@@ -0,0 +1,19 @@
+require 'rake'
+
+PKG_NAME = 'hornetseye-hypercomplex'
+PKG_VERSION = '1.0.2'
+RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
+TC_FILES = FileList[ 'test/tc_*.rb' ]
+TS_FILES = FileList[ 'test/ts_*.rb' ]
+PKG_FILES = [ 'Rakefile', 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + TS_FILES + TC_FILES
+BIN_FILES = [ 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + TS_FILES + TC_FILES
+SUMMARY = %q{Hypercomplex number extension for Hornetseye}
+DESCRIPTION = %q{This Ruby extension implements hypercomplex elements for arrays of type Hornetseye::MultiArray.}
+LICENSE = 'GPL-3+'
+AUTHOR = %q{Jan Wedekind}
+EMAIL = %q{jan@wedesoft.de}
+HOMEPAGE = %q{http://wedesoft.github.com/hornetseye-hypercomplex/}
+
+
